d3355e98fce49fe88aa9d3ed424cf28d4715ba14
Lucas Neto <61164638+lucasnneto@users.noreply.github.com>
2022-09-23 10:56:14 -0300

    feat: deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/do-it/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/do-it/" : "/",
 });
